6e70d4f4b0bc551f86de1f3f6c7c65ed261c71af
Victor Alves <victor.earn@outlook.com>
2020-03-28 14:13:38 -0300

    Inicio do projeto

---
+++ CalculoDeAlturas/CalculoDeAlturas/ConversorDeAlturas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoDeAlturas
+{
+    public static class ConversorDeAlturas
+    {
+        public static double PesParaMetros(double pes) 
+        {
+            return double.MinValue;
+        }
+    }
+}
